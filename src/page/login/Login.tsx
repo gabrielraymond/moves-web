fc74d81e1ac1403b353a3f20cd583b2a8c9f9e20
gabrielraymond <gabrielraymond.grd@gmail.com>
2022-03-15 17:36:52 +0700

    progress login

--- src/page/login/Login.tsx
+++ src/page/login/Login.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../components/Button/Button";
 import "./Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const data = {
+      email: email,
+      password: password,
+    };
+    console.log(data);
+  };
+
   return (
     <section
       id="login"
@@ -11,13 +23,16 @@ const Login = () => {
       <div className="login-wrapper text-center">
         <img src="/Asset/Login/MOVES LOGO WHITE-01 1 (1).PNG" alt="Logo" />
         <h3>Welcome To POS Melandas</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <input
               type="email"
               className="form-control"
               id="email"
               placeholder="Enter your email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-3">
@@ -26,6 +41,9 @@ const Login = () => {
               className="form-control"
               id="password"
               placeholder="Enter you password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <Button title="Login" icon="" />
